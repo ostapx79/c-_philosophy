c46f7f5f18327643ed0d40cc564c77c4e8c17cff
Constantin Galus <ostapx79@gmail.com>
2020-11-06 02:57:34 +0300

    упражнения 3

---
+++ C02/exerxcises/exercises3.cpp
@@ -0,0 +1,20 @@
+//: C02/exercises/exercises.cpp
+// Напишите программу, которая открывает файл и подсчитывает количество слов,
+// резделенных прорусками.
+#include <string>
+#include <iostream>
+#include <fstream>
+
+int main() {
+    using namespace std;
+    ifstream in("exercises3.cpp");
+    string s, word;
+    int count = 0;
+    while (in >> word) {
+        s += word + '\n';
+        count++;
+    }
+    cout << s;
+    cout << count << endl;
+    return 0;
+}
