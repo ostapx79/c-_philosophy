db3db8b14943e7bfb19ac224a34d31b76c2d00ca
Constantin Galus <ostapx79@gmail.com>
2020-11-10 01:04:28 +0300

    exercises2_5.cpp, читается наоборот

---
+++ C02/exerxcises/exercises2_5.cpp
@@ -0,0 +1,27 @@
+//: C02/exercises2_5.cpp
+// Измените программу Fillvector.cpp так, чтобы строки выводились в обратном
+// порядке (от последней к первой).
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+int main() {
+    using namespace std;
+    vector<string> vs;
+    ifstream in("exercises2_5.cpp");
+    string line;
+    while (getline(in, line)) {
+        vs.push_back(line);
+    }
+    cout << vs.size() << endl;
+    for (int i = vs.size(); i > 0; i--) {
+        cout << vs[i] << endl;
+    }
+    int numb_i = vs.size();
+    while (numb_i >= 0) {
+        cout << vs[numb_i] << endl;
+        numb_i--;
+    }
+    return 0;
+}
