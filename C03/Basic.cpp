cb429ec3e9d3d38551546298b5663448585da745
Constantin Galus <ostapx79@gmail.com>
2020-11-21 23:00:37 +0300

    типы данных

---
+++ C03/Basic.cpp
@@ -0,0 +1,16 @@
+//: C03/Basic.cpp
+// Определение переменных четырех встроенных типов данных в Си и Си++
+
+int main() {
+    // Определение без инициализации:
+    char protein;
+    int carbohydrates;
+    float fiber;
+    double fat;
+    // Определение, совмещенное с инициализацией:
+    char pizza = 'A', pop = 'Z';
+    int dongdings = 100, twinkles = 150, heehos = 200;
+    float chocolate = 3.14159;
+    // Экспоненциальная запись:
+    double fudge_ripple = 6e-4;
+}
