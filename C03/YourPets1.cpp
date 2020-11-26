a9dad433bde1b5d401f814fe19fc33af9edb66b2
Constantin Galus <ostapx79@gmail.com>
2020-11-27 01:09:20 +0300

    знакомства с указатели

---
+++ C03/YourPets1.cpp
@@ -0,0 +1,12 @@
+//: C03/YourPets1.cpp
+#include <iostream>
+
+int dog, cat, bird, fish;
+
+void f(int pet) {
+    std::cout << "pet id number: " << pet << std::endl;
+}
+int main() {
+    int i, j, k;
+    return 0;
+}
